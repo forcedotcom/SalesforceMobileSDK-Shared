b4c2db0572ca3341c27338ca448645ff9f03bce7
Wolfgang Mathurin <wmathurin@salesforce.com>
2013-03-26 17:22:14 -0700

    Renamed bootstrap.js to auth.js so it doesn't get confused with bootstrap.css / Account editor list is refreshed when navigating back to it / Failed delete shown to user with alert / Validation error shown to user (fields highlighted)

--- sobject/libs/forcebone.js
+++ sobject/libs/forcebone.js
@@ -15,6 +15,29 @@
         forcetkClient.setUserAgentString(creds.userAgent);
     };
 
+    Backbone.Force.RestError = function(xhr) {
+        // 200	“OK” success code, for GET or HEAD request.
+        // 201	“Created” success code, for POST request.
+        // 204	“No Content” success code, for DELETE request.
+        // 300	The value returned when an external ID exists in more than one record. The response body contains the list of matching records.
+        // 400	The request couldn’t be understood, usually because the JSON or XML body contains an error.
+        // 401	The session ID or OAuth token used has expired or is invalid. The response body contains the message and errorCode.
+        // 403	The request has been refused. Verify that the logged-in user has appropriate permissions.
+        // 404	The requested resource couldn’t be found. Check the URI for errors, and verify that there are no sharing issues.
+        // 405	The method specified in the Request-Line isn’t allowed for the resource specified in the URI.
+        // 415	The entity in the request is in a format that’s not supported by the specified method.
+        // 500	An error has occurred within Force.com, so the request couldn’t be completed. Contact salesforce.com Customer Support.
+        this.xhr = xhr;
+        this.status = xhr.status;
+        try {
+            this.details = JSON.parse(xhr.responseText);
+        }
+        catch (e) { 
+            console.log("Could not parse responseText:" + e);
+        }
+    };
+
+
     Backbone.Force.Model = Backbone.Model.extend({
         // Fields that need to be defined on every instance
         sobjectType:null,
@@ -22,46 +45,34 @@
 
         sync: function(method, model, options) {
             var that = this;
+
             if (method == "read")
             {
                 forcetkClient.retrieve(this.sobjectType, this.id, options.fieldlist, 
-                                       function(result) {
-                                           options.success(result);
-                                       },
-                                       function(error) {
-                                           options.error(error);
-                                       });
+                                       options.success,
+                                       options.error);
             }
             else if (method == "create")
             {
                 forcetkClient.create(this.sobjectType, _.omit(this.attributes, 'Id'),
-                                       function(result) {
-                                           that.set('Id', result.id);
-                                           options.success(that);
-                                       },
-                                       function(error) {
-                                           options.error(error);
-                                       });
+                                     function(result) {
+                                         that.set('Id', result.id);
+                                         options.success(that);
+                                     }, 
+                                     options.error);
             }
             else if (method == "update")
             {
-                forcetkClient.update(this.sobjectType, this.id, this.changed,
-                                       function(result) {
-                                           options.success(that);
-                                       },
-                                       function(error) {
-                                           options.error(error);
-                                       });
+                forcetkClient.update(this.sobjectType, this.id, this.changed, 
+                                     options.success,
+                                     options.error);
+                                     
             }
             else if (method == "delete")
             {
                 forcetkClient.del(this.sobjectType, this.id,
-                                  function(result) {
-                                      // TBD
-                                  },
-                                  function(error) {
-                                      options.error(error);
-                                  });
+                                  options.success,
+                                  options.error);
             }
         }
     });
@@ -72,20 +83,20 @@
 
         sync: function(method, model, options) {
             var that = this;
+
             if (method == "read")
             {
+                options.reset = true;
                 if (this.soql != null) {
                     forcetkClient.query(this.soql,
                                         function(results) {
-                                            that.reset(results.records);
+                                            options.success(results.records);
                                         },
-                                        function(error) {
-                                            options.error(error);
-                                        });
+                                        options.error);
                 }
                 else 
                 {
-                    // TBD
+                    options.success([]);
                 }
             }
             else
