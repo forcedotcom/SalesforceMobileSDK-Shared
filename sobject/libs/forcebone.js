23bebb29aad018cb3dc8e171d172cd3cc02bda82
Wolfgang Mathurin <wmathurin@salesforce.com>
2013-04-01 18:19:35 -0700

    Beginning of smartstore integration in forcebone.js

--- sobject/libs/forcebone.js
+++ sobject/libs/forcebone.js
@@ -22,7 +22,6 @@
     // Function to turn methods with callbacks into jQuery promises
     var promiser = function(object, methodName) {
         var retfn = function () {
-            var self = this;
             var args = $.makeArray(arguments);
             var d = $.Deferred();
             args.push(function() {
@@ -40,6 +39,9 @@
     // Private forcetk client with promise-wrapped methods
     var forcetkClient;
 
+    // Private smartstore client with promise-wrapped methods
+    var smartstoreClient; 
+
     // Init function
     Force.init = function(creds, apiVersion) {
         var innerForcetkClient = new forcetk.Client(creds.clientId, creds.loginUrl);
@@ -54,6 +56,12 @@
         forcetkClient.del = promiser(innerForcetkClient, "del");
         forcetkClient.query = promiser(innerForcetkClient, "query");
         forcetkClient.search = promiser(innerForcetkClient, "search");
+
+        smartstoreClient = new Object();
+        smartstoreClient.registerSoup = promiser(navigator.smartstore, "registerSoup");
+        smartstoreClient.upsertSoupEntries = promiser(navigator.smartstore, "upsertSoupEntries");
+        smartstoreClient.retrieveSoupEntries = promiser(navigator.smartstore, "retrieveSoupEntries");
+        smartstoreClient.removeFromSoup = promiser(navigator.smartstore, "removeFromSoup");
     };
 
     Force.RestError = function(xhr) {
@@ -79,8 +87,10 @@
     };
 
 
+    // Force.Model
+    // --------------
     Force.Model = Backbone.Model.extend({
-        // NB: sobjectType is expected on every instance
+        // sobjectType is expected on every instance
         sobjectType:null,
         idAttribute: 'Id',
 
@@ -90,6 +100,7 @@
 
         // Pass a fieldlist in options if you don't want to fetch the whole record
         // Pass a refetch:true in options during save do refetch record from server
+        // Pass a local:true in options to keep smartstore in sync
         sync: function(method, model, options) {
             var promise = null;
             switch(method) {
@@ -102,51 +113,88 @@
             case "delete": promise = forcetkClient.del(model.sobjectType, model.id); break;
             }
 
-            if (options.refetch) {
-                promise = promise.then(function() { return forcetkClient.retrieve(model.sobjectType, model.id, options.fieldlist); });
+            if (options.refetch)
+            {
+                promise = promise.then(function() { 
+                    return forcetkClient.retrieve(model.sobjectType, model.id, options.fieldlist); 
+                });
+            }
+
+            if (options.local) {
+                promise = promise.then(function() { 
+                    if (method == "delete") {
+                        return smartstoreClient.removeFromSoup(model.getClass().soupName, [ model.id ]);
+                    }
+                    else {
+                        return smartstoreClient.upsertSoupEntries(model.getClass().soupName, [ model ]);
+                    }
+                });
             }
 
             promise.done(options.success).fail(options.error);
         }
+    },{
+        // soupName: backing soup to which models are saved when local:true is specified during CRUD operation
+        soupName: null,
+
+        setupSoup: function(soupName, fieldlist) {
+            this.soupName = soupName;
+            var indexSpecs = [];
+            _.each(fieldlist, function(field) { indexSpecs.push({path: field, type:"string"}); });
+            return smartstoreClient.registerSoup(soupName, indexSpecs)
+        }
     });
 
+    // Force.Collection
+    // ----------------
     Force.Collection = Backbone.Collection.extend({
         soql:null,
         sosl:null,
 
+        // Pass a local:true in options to keep smartstore in sync
         sync: function(method, model, options) {
-            if (method == "read")
-            {
+            var promise = null;
+            switch(method) {
+            case "read": 
                 options.reset = true;
                 if (this.soql != null) {
-                    forcetkClient.query(this.soql)
-                    .done(function(results) {
-                        options.success(results.records);
-                    })
-                    .fail(options.error);
+                    promise = forcetkClient.query(this.soql).then(function(results) { return results.records; });
                 }
                 else if (this.sosl != null) {
-                    forcetkClient.search(this.sosl)
-                        .done(function(results) {
-                            var records = [];
-                            _.each(results, function(result) {
-                                var sobjectType = result.attributes.type;
-                                var sobject = new model.model(_.omit(result, 'attributes'));
-                                sobject.sobjectType = sobjectType;
-                                records.push(sobject);
-                            });
-                            options.success(records);
-                        })
-                    .fail(options.error);
+                    promise = forcetkClient.search(this.sosl);
                 }
-                else 
-                {
-                    options.success([]);
+
+                if (promise != null) {
+                    promise = promise.then(function(results) {
+                        var records = [];
+                        _.each(results, function(result) {
+                            var sobjectType = result.attributes.type;
+                            var sobject = new model.model(_.omit(result, 'attributes'));
+                            sobject.sobjectType = sobjectType;
+                            records.push(sobject);
+                        });
+                        return records;
+                    });
                 }
+
+                break;
+            }
+
+            if (promise != null)
+            {
+                if (options.local) {
+                    promise = promise.then(function(records) { 
+                        return smartstoreClient.upsertSoupEntries(model.model.getClass().soupName, records);
+                    });
+                }
+
+                promise.done(function(data) {
+                    options.success(data);
+                }).fail(options.error);
             }
             else
             {
-                // TBD
+                options.success([]);
             }
         }
     });
