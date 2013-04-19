d8d42f77bfbfc522fdc0ae2e37afde58cbc35705
Wolfgang Mathurin <wmathurin@salesforce.com>
2013-04-18 17:02:14 -0700

    To switch from container to local, you only have to change the html (split auth.js into auth.js and auth_local.js)

--- SampleApps/entity/auth.js
+++ SampleApps/entity/auth.js
@@ -8,24 +8,7 @@ var app = {
 jQuery(document).ready(function() {
     //Add event listeners and so forth here
     console.log("onLoad: jquery ready");
-
-    // Use following when in container
-	// document.addEventListener("deviceready", onDeviceReady,false);
-    // End container
-
-    // Use  when testing in browser
-    // Also make sure to start browser with same origin policy disable
-    // See http://joshuamcginnis.com/2011/02/28/how-to-disable-same-origin-policy-in-chrome/
-    var creds = {
-        accessToken: "--will-be-obtained-by-refres",
-        refreshToken: "5Aep861_OKMvio5gy9sGt9Z9mdt62xXK.9ugif6nZJYknXeANTICBf4ityN9j6YDgHjFvbzu6FTUQ==",
-        clientId: "3MVG92.uWdyphVj4bnolD7yuIpCQsNgddWtqRND3faxrv9uKnbj47H4RkwheHA2lKY4cBusvDVp0M6gdGE8hp",
-        loginUrl: "https://test.salesforce.com",
-        instanceUrl: "https://tapp0.salesforce.com",
-        accountAgent: "sobject-proto"
-    };
-    appStart(creds);
-    // End local testing
+	document.addEventListener("deviceready", onDeviceReady,false);
 });
 
 // When this function is called, cordova has been initialized and is ready to roll 
