4c6adb85a4442370832035a1eb550f1f2e3c6e72
Wolfgang Mathurin <wmathurin@salesforce.com>
2013-04-19 11:24:53 -0700

    Taking refresh token out

--- SampleApps/entity/auth_local.js
+++ SampleApps/entity/auth_local.js
@@ -13,8 +13,8 @@ jQuery(document).ready(function() {
     // Also make sure to start browser with same origin policy disable
     // See http://joshuamcginnis.com/2011/02/28/how-to-disable-same-origin-policy-in-chrome/
     var creds = {
-        accessToken: "--will-be-obtained-by-refres",
-        refreshToken: "5Aep861_OKMvio5gy9sGt9Z9mdt62xXK.9ugif6nZJYknXeANTICBf4ityN9j6YDgHjFvbzu6FTUQ==",
+        accessToken: "--will-be-obtained-by-refresh--",
+        refreshToken: "--insert-a-valid-refresh-token-here--",
         clientId: "3MVG92.uWdyphVj4bnolD7yuIpCQsNgddWtqRND3faxrv9uKnbj47H4RkwheHA2lKY4cBusvDVp0M6gdGE8hp",
         loginUrl: "https://test.salesforce.com",
         instanceUrl: "https://tapp0.salesforce.com",
