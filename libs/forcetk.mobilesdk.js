25ab5e4899d8acbce7d0e7d0f09d300da7430d59
Bharath Hariharan <bhariharan@salesforce.com>
2013-07-10 15:25:47 -0700

    'unstable' is now live

--- libs/forcetk.mobilesdk.js
+++ libs/forcetk.mobilesdk.js
@@ -25,7 +25,7 @@
  */
 
 // Version this js was shipped with
-var SALESFORCE_MOBILE_SDK_VERSION = "2.0.0";
+var SALESFORCE_MOBILE_SDK_VERSION = "2.0.1.unstable";
 
 /*
  * JavaScript library to wrap REST API on Visualforce. Leverages Ajax Proxy
--- test/MockSDKInfo.js
+++ test/MockSDKInfo.js
@@ -45,7 +45,7 @@ var MockSDKInfo = (function(window) {
             var self = this;
 
             cordova.interceptExec(SDKINFO_SERVICE, "getInfo", function (successCB, errorCB, args) {
-                successCB(new SDKInfo("2.0.0", 
+                successCB(new SDKInfo("2.0.1.unstable", 
                                       ["com.salesforce.oauth", "com.salesforce.sdkinfo", "com.salesforce.testrunner", "com.salesforce.smartstore"], 
                                       "ForcePluginsTest", "1.0"));
             });
