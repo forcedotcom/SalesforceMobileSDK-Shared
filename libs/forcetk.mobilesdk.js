4f8d971a17a294c4f998b4057ce625ffba391338
Bharath Hariharan <bhariharan@salesforce.com>
2013-11-05 17:07:14 -0800

    Change version number to 2.1.0

--- libs/forcetk.mobilesdk.js
+++ libs/forcetk.mobilesdk.js
@@ -27,7 +27,7 @@
 (function($j) {
 
 // Version this js was shipped with
-this.SALESFORCE_MOBILE_SDK_VERSION = "2.1.0.unstable";
+this.SALESFORCE_MOBILE_SDK_VERSION = "2.1.0";
 
 /*
  * JavaScript library to wrap REST API on Visualforce. Leverages Ajax Proxy
