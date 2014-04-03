8f27d9e829a1df1e8642e282227e6d91aa0d3881
Wolfgang Mathurin <wmathurin@salesforce.com>
2014-04-02 21:24:15 -0700

    Better grunt file (removes bower_components folder)

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
             install: {
                 options: {
                     targetDir: 'dependencies',
-                    cleanTargetDir: true
+                    cleanup: true
                 }
             }
         }
