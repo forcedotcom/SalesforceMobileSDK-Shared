forcedotcom/SalesforceMobileSDK-Shared

commit 288fa8be6f977694b7ada5d7f3f078975f3da38e

don't fail if push plugin not there

gen/plugins/com.salesforce/com.salesforce.util.push.js
@@ -31,7 +31,7 @@ var SALESFORCE_MOBILE_SDK_VERSION = "2.3.0.unstable";
  * Register push notification handler
  */
 var registerPushNotificationHandler = function(notificationHandler, fail) {
-    if (typeof window.plugins.pushNotification === "undefined") {
+    if (window.plugins && window.plugins.pushNotification) {
         console.err("PushPlugin not found");
         fail("PushPlugin not found");
         return;

gen/plugins_with_define/com.salesforce/com.salesforce.util.push.js
@@ -32,7 +32,7 @@ var SALESFORCE_MOBILE_SDK_VERSION = "2.3.0.unstable";
  * Register push notification handler
  */
 var registerPushNotificationHandler = function(notificationHandler, fail) {
-    if (typeof window.plugins.pushNotification === "undefined") {
+    if (window.plugins && window.plugins.pushNotification) {
         console.err("PushPlugin not found");
         fail("PushPlugin not found");
         return;

libs/cordova.force.js
@@ -785,7 +785,7 @@ cordova.define("com.salesforce.util.push", function(require, exports, module) {
      * Register push notification handler
      */
     var registerPushNotificationHandler = function(notificationHandler, fail) {
-        if (typeof window.plugins.pushNotification === "undefined") {
+        if (window.plugins && window.plugins.pushNotification) {
             console.err("PushPlugin not found");
             fail("PushPlugin not found");
             return;

commit 6c8b4da9c0d73dfe7201217430e932340b2e83df

Fixing regression introduced by 288fa8be6f977694b7ada5d7f3f078975f3da38e

gen/plugins/com.salesforce/com.salesforce.util.push.js
@@ -31,7 +31,7 @@ var SALESFORCE_MOBILE_SDK_VERSION = "2.3.0";
  * Register push notification handler
  */
 var registerPushNotificationHandler = function(notificationHandler, fail) {
-    if (window.plugins && window.plugins.pushNotification) {
+    if (!window.plugins || !window.plugins.pushNotification) {
         console.err("PushPlugin not found");
         fail("PushPlugin not found");
         return;

gen/plugins_with_define/com.salesforce/com.salesforce.util.push.js
@@ -32,7 +32,7 @@ var SALESFORCE_MOBILE_SDK_VERSION = "2.3.0";
  * Register push notification handler
  */
 var registerPushNotificationHandler = function(notificationHandler, fail) {
-    if (window.plugins && window.plugins.pushNotification) {
+    if (!window.plugins || !window.plugins.pushNotification) {
         console.err("PushPlugin not found");
         fail("PushPlugin not found");
         return;

libs/cordova.force.js
@@ -786,7 +786,7 @@ cordova.define("com.salesforce.util.push", function(require, exports, module) {
      * Register push notification handler
      */
     var registerPushNotificationHandler = function(notificationHandler, fail) {
-        if (window.plugins && window.plugins.pushNotification) {
+        if (!window.plugins || !window.plugins.pushNotification) {
             console.err("PushPlugin not found");
             fail("PushPlugin not found");
             return;