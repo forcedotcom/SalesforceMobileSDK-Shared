2192e276f8b267a2833274a911a8117d8f728c35
Wolfgang Mathurin <wmathurin@salesforce.com>
2016-01-22 18:01:21 -0800

    Ordering by name (otherwise results are ordered by the non-leaf node)

--- test/SFSmartStoreTestSuite.js
+++ test/SFSmartStoreTestSuite.js
@@ -998,7 +998,7 @@ SmartStoreTestSuite.prototype.testLikeQueryAgainstArrayNode  = function() {
             return self.upsertSoupEntries(soupName,rawEntries);
         })
         .pipe(function(entries) {
-            var querySpec = navigator.smartstore.buildLikeQuerySpec("attributes.color", "%grey%");
+            var querySpec = navigator.smartstore.buildLikeQuerySpec("attributes.color", "%grey%", "descending", 10, "name");
             return self.tryQuery(soupName, querySpec, ["elephant", "dog"]);
         })
         .pipe(function() {
