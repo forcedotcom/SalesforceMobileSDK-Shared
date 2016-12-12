4fd46c19a594d3afa4a21209fe7313bf767d05a8
Bharath Hariharan <bhariharan@salesforce.com>
2016-12-12 12:36:22 -0800

    Merging back from master to unstable

--- gen/plugins_with_define/com.salesforce/com.salesforce.plugin.network.js
+++ gen/plugins_with_define/com.salesforce/com.salesforce.plugin.network.js
@@ -26,7 +26,7 @@ cordova.define("com.salesforce.plugin.network", function(require, exports, modul
  */
 
 // Version this js was shipped with
-var SALESFORCE_MOBILE_SDK_VERSION = "5.0.0";
+var SALESFORCE_MOBILE_SDK_VERSION = "5.1.0";
 var SERVICE = "com.salesforce.network";
 var exec = require("com.salesforce.util.exec").exec;
 
