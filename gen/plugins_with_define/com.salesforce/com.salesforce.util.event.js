911f8060e71384fcb5b7260694d9cb827f4f674c
Wolfgang Mathurin <wmathurin@salesforce.com>
2021-05-20 12:13:19 -0700

    Updating version numbers to 9.2.0

--- gen/plugins_with_define/com.salesforce/com.salesforce.util.event.js
+++ gen/plugins_with_define/com.salesforce/com.salesforce.util.event.js
@@ -26,7 +26,7 @@ cordova.define("com.salesforce.util.event", function(require, exports, module) {
  */
 
 // Version this js was shipped with
-var SALESFORCE_MOBILE_SDK_VERSION = "9.1.0";
+var SALESFORCE_MOBILE_SDK_VERSION = "9.2.0";
 
 var logger = require("com.salesforce.util.logger");
 
