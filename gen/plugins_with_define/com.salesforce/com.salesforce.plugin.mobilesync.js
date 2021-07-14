365ae29bb027ca13cd799f498f9d5e96e1578315
Wolfgang Mathurin <wmathurin@salesforce.com>
2021-07-14 10:21:52 -0600

    Updating version numbers to 9.2.0

--- gen/plugins_with_define/com.salesforce/com.salesforce.plugin.mobilesync.js
+++ gen/plugins_with_define/com.salesforce/com.salesforce.plugin.mobilesync.js
@@ -26,7 +26,7 @@ cordova.define("com.salesforce.plugin.mobilesync", function(require, exports, mo
  */
 
 // Version this js was shipped with
-var SALESFORCE_MOBILE_SDK_VERSION = "9.1.1";
+var SALESFORCE_MOBILE_SDK_VERSION = "9.2.0";
 var SERVICE = "com.salesforce.mobilesync";
 
 var exec = require("com.salesforce.util.exec").exec;
