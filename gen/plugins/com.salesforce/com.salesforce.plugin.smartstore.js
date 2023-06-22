07e73145e29510675be7f68a0191ac9d39dc7195
Wolfgang Mathurin <wmathurin@salesforce.com>
2023-06-22 15:06:24 -0700

    Updating version numbers to 11.1.0

--- gen/plugins/com.salesforce/com.salesforce.plugin.smartstore.js
+++ gen/plugins/com.salesforce/com.salesforce.plugin.smartstore.js
@@ -25,7 +25,7 @@
  */
 
 // Version this js was shipped with
-var SALESFORCE_MOBILE_SDK_VERSION = "11.0.0";
+var SALESFORCE_MOBILE_SDK_VERSION = "11.1.0";
 var SERVICE = "com.salesforce.smartstore";
 
 var exec = require("com.salesforce.util.exec").exec;
