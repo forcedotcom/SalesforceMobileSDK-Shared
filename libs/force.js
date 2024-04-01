66a1c8163a8be846c5d98211a7b8e5d493a2d91a
Wolfgang Mathurin <wmathurin@salesforce.com>
2024-04-01 15:42:03 -0700

    Moving to v60.0

--- libs/force.js
+++ libs/force.js
@@ -42,7 +42,7 @@ var force = (function () {
 
     // The force.com API version to use.
     // To override default, pass apiVersion in init(props)
-        apiVersion = 'v59.0',
+        apiVersion = 'v60.0',
 
     // Keep track of OAuth data (access_token, refresh_token, instance_url and user_id)
         oauth,
