a14efcef9b85cd4fff7d174df31b33c3787d37aa
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-09-23 16:04:14 -0700

    Updating version numbers to 14.0.0

--- gen/plugins/com.salesforce/com.salesforce.util.push.js
+++ gen/plugins/com.salesforce/com.salesforce.util.push.js
@@ -25,7 +25,7 @@
  */
 
 // Version this js was shipped with
-var SALESFORCE_MOBILE_SDK_VERSION = "14.0.0-rc.2";
+var SALESFORCE_MOBILE_SDK_VERSION = "14.0.0";
 
 /**
  * Register push notification handler.
